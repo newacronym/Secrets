30df7d64b9bed4dd5101df7ea805195b50dee4dd
harshit <harshitgpt07@gmail.com>
2021-11-12 15:37:29 +0530

    final

--- app.js
+++ app.js
@@ -75,8 +75,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://frozen-brushlands-38603.herokuapp.com",
-
+    callbackURL: "https://frozen-brushlands-38603.herokuapp.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile.displayName);
